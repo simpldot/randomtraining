c5efb789411fa92a76c021f356a1790a72720b68
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-11 18:27:23 +0100

    added trailing icon

--- lib/widgets/trainingCard.dart
+++ lib/widgets/trainingCard.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 Widget trainingCard(BuildContext context, Map<String, String> data) {
-  return Card(
-    clipBehavior: Clip.antiAlias,
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
     key: Key(data["id"]),
-    child: ListTile(
-      onTap: () {},
-      title: Text(data["title"]),
-      subtitle: Text(data["desc"]),
+    child: Card(
+      clipBehavior: Clip.antiAlias,
+      child: ListTile(
+        onTap: () {},
+        title: Text(data["title"]),
+        subtitle: Text(data["desc"]),
+        trailing: Icon(Icons.drag_handle_rounded),
+      ),
     ),
   );
 }
