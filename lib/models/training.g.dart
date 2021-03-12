5b1164206de08afe33f829ce880b8fc74e6b5b2e
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-12 15:15:40 +0100

    remove id adapter

--- lib/models/training.g.dart
+++ lib/models/training.g.dart
@@ -16,24 +16,22 @@ class TrainingAdapter extends TypeAdapter<Training> {
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return Training()
-      ..id = fields[0] as String
-      ..title = fields[1] as String
-      ..desc = fields[2] as String
-      ..blocks = (fields[3] as List)?.cast<Block>();
+    return Training(
+      fields[0] as String,
+      fields[1] as String,
+      (fields[2] as List)?.cast<Block>(),
+    );
   }
 
   @override
   void write(BinaryWriter writer, Training obj) {
     writer
-      ..writeByte(4)
-      ..writeByte(0)
-      ..write(obj.id)
-      ..writeByte(1)
-      ..write(obj.title)
-      ..writeByte(2)
-      ..write(obj.desc)
       ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.desc)
+      ..writeByte(2)
       ..write(obj.blocks);
   }
 
