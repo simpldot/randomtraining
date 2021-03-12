6f6cc1e5c9c56214dd0cd891d14f27a3a8272767
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-12 02:37:26 +0100

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'views/home/homeView.dart';
-import 'views/settings/themeChanger.dart';
-import 'views/settings/themes.dart';
+import 'views/settings/themeController.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  await Hive.initFlutter();
+  final Box<dynamic> db = await Hive.openBox('appDB');
+  runApp(MyApp(database: db));
 }
 
 class MyApp extends StatelessWidget {
+  final Box<dynamic> database;
+  MyApp({this.database});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<ThemeChanger>(
-          create: (context) => ThemeChanger(getThemeData('defaultDark')),
-        ),
+        ChangeNotifierProvider(create: (_) => ThemeController(database)),
       ],
       child: MaterialAppWithTheme(),
     );
@@ -25,7 +29,7 @@ class MyApp extends StatelessWidget {
 class MaterialAppWithTheme extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeChanger>(context);
+    final theme = Provider.of<ThemeController>(context);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
