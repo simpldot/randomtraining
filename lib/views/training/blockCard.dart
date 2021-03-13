a82454a32c6427cf61250f9dffbf7099704068f6
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-13 02:49:14 +0100

    update card

--- lib/views/training/blockCard.dart
+++ lib/views/training/blockCard.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:randomtraining/models/block.dart';
 import 'package:randomtraining/shared/textStyles.dart';
 import 'package:randomtraining/views/block/blockView.dart';
 
-Widget blockCard(BuildContext context, Map<String, String> data) {
+Widget blockCard(BuildContext context, Block block, int id) {
+  String idString = "bl-" + id.toString();
   return Padding(
     padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-    key: Key(data["id"]),
+    key: Key(idString),
     child: Card(
       clipBehavior: Clip.antiAlias,
       child: ListTile(
         onTap: () {
           Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => BlockView(id: data["id"])));
+              builder: (BuildContext context) => BlockView(id: idString)));
         },
         title: Hero(
-            tag: data["title"],
-            child: Material(child: Text(data["title"], style: smallHeading))),
+            tag: idString + "t",
+            child: Material(child: Text(block.title, style: smallHeading))),
         subtitle:
-            Hero(tag: data["desc"], child: Material(child: Text(data["desc"]))),
+            Hero(tag: idString + "d", child: Material(child: Text(block.desc))),
         trailing: Icon(Icons.drag_handle_rounded),
       ),
     ),
