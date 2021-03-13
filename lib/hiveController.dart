9328b80c5aeac3f5596001c2eeb3abbfd5f1ef05
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-13 02:46:27 +0100

    add currentTraining

--- lib/hiveController.dart
+++ lib/hiveController.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
+import 'models/block.dart';
 import 'models/training.dart';
 
 class HiveController extends ChangeNotifier {
   Box trainingsBox;
   final String _trainingsKey = 'trainings';
   List<Training> _trainings;
+  Training currentTraining;
 
   HiveController(trainingsBox) {
     this.trainingsBox = trainingsBox;
     _trainings = trainingsBox.containsKey(_trainingsKey)
         ? trainingsBox.get(_trainingsKey)
         : [];
+    print(_trainings);
   }
 
   List<Training> get trainings => _trainings;
@@ -20,8 +23,12 @@ class HiveController extends ChangeNotifier {
   addTraining(String title, String desc) {
     Training newTraining = Training(title, desc, []);
     _trainings.add(newTraining);
-    print(_trainings);
     trainingsBox.add(newTraining);
+    notifyListeners();
+  }
+
+  addBlock(String title, String desc) {
+    currentTraining.blocks.add(Block(title, desc, []));
     notifyListeners();
   }
 }
