16f9406eaaac7a162af352a77b8ad598cba4d283
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-15 17:41:18 +0100

    add settings

--- lib/views/home/homeView.dart
+++ lib/views/home/homeView.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:randomtraining/controllers/trainingController.dart';
 import 'package:randomtraining/models/training.dart';
 import 'package:randomtraining/shared/textStyles.dart';
 import 'package:randomtraining/views/home/addTrainingView.dart';
 import 'package:randomtraining/views/home/trainingCard.dart';
+import 'package:randomtraining/views/settings/settingsView.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key key}) : super(key: key);
@@ -15,15 +17,11 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   List<Training> trainings;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    trainings = Provider.of<TrainingController>(context).trainings;
+    Box trainingsBox = Provider.of<TrainingController>(context).trainingsBox;
+    trainings = trainingsBox.values.toList();
+    List<int> trainingsKeys = trainingsBox.keys.toList().cast<int>();
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -33,14 +31,18 @@ class _HomeViewState extends State<HomeView> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.settings),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => SettingsView()));
+            },
           ),
         ],
       ),
       body: ReorderableListView.builder(
         onReorder: _onReorder,
         itemCount: trainings.length,
-        itemBuilder: (context, i) => trainingCard(context, trainings[i], i),
+        itemBuilder: (context, i) =>
+            trainingCard(context, trainings[i], trainingsKeys[i]),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
