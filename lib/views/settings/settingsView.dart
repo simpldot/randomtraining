a0ea6ec90606d2caade67f8a7df58e63a4be1168
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-20 22:05:42 +0100

    add theme selector

--- lib/views/settings/settingsView.dart
+++ lib/views/settings/settingsView.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:randomtraining/controllers/blockController.dart';
-import 'package:randomtraining/controllers/trainingController.dart';
-import 'package:randomtraining/models/training.dart';
+import 'package:randomtraining/controllers/themeController.dart';
 import 'package:randomtraining/shared/textStyles.dart';
-import 'package:randomtraining/views/training/addBlockView.dart';
-import 'package:randomtraining/views/training/blockCard.dart';
+import 'package:randomtraining/views/settings/themes.dart';
 
 class SettingsView extends StatefulWidget {
   SettingsView({Key key}) : super(key: key);
@@ -15,8 +12,19 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
+  List<String> themes = [
+    'defaultDark',
+    'defaultLight',
+    'greenDark',
+    'greenLight',
+    'pinkDark',
+    'pinkLight'
+  ];
+  List<bool> isSelected = List.generate(6, (_) => false);
   @override
   Widget build(BuildContext context) {
+    ThemeController themeController = Provider.of<ThemeController>(context);
+    isSelected[themes.indexOf(themeController.selectedTheme)] = true;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -24,19 +32,48 @@ class _SettingsViewState extends State<SettingsView> {
         centerTitle: true,
         title: Text("Settings", style: heading),
       ),
-      body: Column(
-        children: [
-          Text("test"),
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Themes",
+              style: smallHeading,
+            ),
+            ToggleButtons(
+              children: [
+                for (String theme in themes)
+                  Container(
+                    color: getThemeData(theme).backgroundColor,
+                    child: Icon(
+                      Icons.stop_rounded,
+                      size: 50,
+                      color: getThemeData(theme).primaryColor,
+                    ),
+                  ),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  for (int buttonIndex = 0;
+                      buttonIndex < isSelected.length;
+                      buttonIndex++) {
+                    if (buttonIndex == index) {
+                      themeController.setTheme(themes[buttonIndex]);
+                      isSelected[buttonIndex] = true;
+                    } else {
+                      isSelected[buttonIndex] = false;
+                    }
+                  }
+                });
+              },
+              selectedBorderColor: Colors.red,
+              isSelected: isSelected,
+              borderWidth: 3,
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Provider.of<TrainingController>(context, listen: false).deleteAll();
-        },
-        label: Text('deleteAll'),
-        icon: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
