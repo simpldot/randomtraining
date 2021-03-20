bebddbf1af8ef22654d2b494a5cb00d8bcf58e88
joscha0 <45885696+joscha0@users.noreply.github.com>
2021-03-20 19:28:58 +0100

    add more themes

--- lib/views/settings/themes.dart
+++ lib/views/settings/themes.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
+ThemeData _getThemeData(Brightness brightness, Color primaryColor,
+    Color backgroundColor, Color textColor) {
+  return ThemeData(
+    brightness: brightness,
+    primaryColor: primaryColor,
+    accentColor: primaryColor,
+    canvasColor: Colors.transparent,
+    scaffoldBackgroundColor: backgroundColor,
+    backgroundColor: backgroundColor,
+    primaryTextTheme: TextTheme(headline6: TextStyle(color: textColor)),
+    primaryIconTheme: IconThemeData(
+      color: textColor,
+    ),
+    toggleableActiveColor: primaryColor,
+  );
+}
+
 ThemeData getThemeData(String themeName) {
   switch (themeName) {
     case 'defaultDark':
-      var primaryColor = Color.fromRGBO(93, 133, 255, 1); //#5d85ff
-      var backgroundColor = Colors.black;
-      return ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: primaryColor,
-        accentColor: primaryColor,
-        canvasColor: Colors.transparent,
-        scaffoldBackgroundColor: backgroundColor,
-        toggleableActiveColor: primaryColor,
-      );
+      Color primaryColor = Color.fromRGBO(93, 133, 255, 1); //#5d85ff
+      Color backgroundColor = Colors.black;
+      Color textColor = Colors.white;
+      Brightness brightness = Brightness.dark;
+      return _getThemeData(
+          brightness, primaryColor, backgroundColor, textColor);
       break;
+    case 'defaultLight':
+      Color primaryColor = Color.fromRGBO(93, 133, 255, 1); //#5d85ff
+      Color backgroundColor = Colors.white;
+      Color textColor = Colors.black;
+      Brightness brightness = Brightness.light;
+      return _getThemeData(
+          brightness, primaryColor, backgroundColor, textColor);
+      break;
+    case 'greenDark':
+      Color primaryColor = Color.fromRGBO(93, 255, 98, 1); //#5dff62
+      Color backgroundColor = Colors.black;
+      Color textColor = Colors.white;
+      Brightness brightness = Brightness.dark;
+      return _getThemeData(
+          brightness, primaryColor, backgroundColor, textColor);
+      break;
+    case 'greenLight':
+      Color primaryColor = Color.fromRGBO(93, 255, 98, 1); //#5dff62
+      Color backgroundColor = Colors.white;
+      Color textColor = Colors.black;
+      Brightness brightness = Brightness.light;
+      return _getThemeData(
+          brightness, primaryColor, backgroundColor, textColor);
     default:
       return ThemeData();
   }
